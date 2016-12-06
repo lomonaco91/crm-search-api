59d6665a06cf8abdd90fa2129b4760f64dfdd8b6
Fabiano Gonçalves Lomonaco Junior <fabiano.junior@local.inatel.br>
2016-12-06 13:47:47 -0200

    Change connection DB

--- connection.js
+++ connection.js
@@ -2,11 +2,11 @@ var mysql = require('mysql');
 
 //Dados para a conexão com o banco de dados MYSQL
 var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user:  process.env.DB_USER,
-    password :  process.env.DB_PASSWORD,
-    port :  process.env.DB_PORT,
-    database:process.env.DB_DATABASE
+    host: process.env.OPENSHIFT_MYSQL_DB_HOST,
+    user:  process.env.OPENSHIFT_MYSQL_DB_USER,
+    password :  process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+    port :  process.env.DB_OPENSHIFT_MYSQL_DB_PORT,
+    database: process.env.OPENSHIFT_MYSQL_DB_DATABASE
 });
 
 //Teste de conexão com o banco, antes de subir o servidor
