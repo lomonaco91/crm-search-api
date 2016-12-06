a7d6d6786b91168c10f457fd4366d76f2a2a2b64
Fabiano Júnior <fabiano.lomonaco.junior@gmail.com>
2016-12-06 19:44:23 -0200

    Fixed conneection

--- connection.js
+++ connection.js
@@ -2,10 +2,9 @@ var mysql = require('mysql');
 
 //Dados para a conexão com o banco de dados MYSQL
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user:  'root',
-    password : 'fabiano123',
-    port :  3306,
+    host: process.env.OPENSHIFT_MYSQL_DB_HOST,
+    user: process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+    password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
     database: 'crmdatabse'
 });
 
